c961b2b2eea86d3e82ce0c3e086115a2fd4690fc
shiftybody <shiftybody@gmail.com>
2024-06-04 16:47:24 -0600

    moch: tailwinds

--- packages/web-client/src/App.jsx
+++ packages/web-client/src/App.jsx
@@ -1,35 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '../public/vite.svg'
+import { Button } from 'flowbite-react'
 import './App.css'
 
-function App () {
-  const [count, setCount] = useState(0)
-
+export default function App () {
   return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='justify-center items-center h-screen flex'>
+      <Button>Hello World</Button>
+    </div>
   )
 }
-
-export default App
