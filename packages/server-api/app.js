29101f8394cecfae40abc9d3af60308b6324ecec
shiftybody <shiftybody@gmail.com>
2024-06-04 16:03:36 -0600

    boilerplate for monorepo comunication

--- packages/server-api/app.js
+++ packages/server-api/app.js
@@ -1,13 +1,28 @@
 import express from 'express'
 import morgan from 'morgan'
 import cookieParser from 'cookie-parser'
+import 'dotenv/config'
 
 import authRoutes from './routes/auth.routes.js'
+import taksRoutes from './routes/tasks.routes.js'
 
 const app = express()
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(cookieParser())
 app.use('/api', authRoutes)
+
+app.use('/api/auth', authRoutes)
+app.use('/api', taksRoutes)
+
+// si NODE_ENV es production use.static para servir el cliente
+// si no, no lo hagas y solo sirve la API
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('../web-client/dist'))
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running')
+  })
+}
 
 export default app
--- packages/server-api/index.js
+++ packages/server-api/index.js
@@ -1,13 +1,16 @@
-// express boilerplate with js with import syntax
-
-// dotenv config
 import 'dotenv/config'
-
 import app from './app.js'
 import { connect } from './db.js'
 
-connect()
+async function main () {
+  try {
+    await connect()
+    app.listen(process.env.PORT)
+    console.log(`Listening on port http://localhost:${process.env.PORT}`)
+    console.log(`Environment: ${process.env.NODE_ENV}`)
+  } catch (error) {
+    console.error(error)
+  }
+}
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server listening on http://localhost:${process.env.PORT}`)
-})
+main()
