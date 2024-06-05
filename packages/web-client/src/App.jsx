30b084d0d8089ad79ac58122c67558c07cf8c234
shiftybody <shiftybody@gmail.com>
2024-06-04 21:29:32 -0600

    create first axios request for register

--- packages/web-client/src/App.jsx
+++ packages/web-client/src/App.jsx
@@ -1,10 +1,20 @@
-import { Button } from 'flowbite-react'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import DashboardPage from './pages/DashboardPage'
 
-export default function App () {
+function App () {
   return (
-    <div className='justify-center items-center h-screen flex'>
-      <Button>Hello World</Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/dashboard' element={<DashboardPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
+
+export default App
