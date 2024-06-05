9e831852e4dc795954f4e86d36d565903877c026
shiftybody <shiftybody@gmail.com>
2024-06-04 21:49:55 -0600

    issued baseURL api refuse

--- packages/web-client/src/api/auth.js
+++ packages/web-client/src/api/auth.js
@@ -1,3 +1,10 @@
 import axios from 'axios'
 
+// if NODE_ENV is production use the production URL
+// if not, use the development URL
+
+if (process.env.NODE_ENV === 'development') {
+  axios.defaults.baseURL = 'http://localhost:3000'
+}
+
 export const registerRequest = user => axios.post('/api/register', user)
